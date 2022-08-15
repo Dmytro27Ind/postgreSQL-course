e8b9cf53ba941cfed9974c03e4003a17c8cc0d9a
Dmytro Kagirov <dmytro27kagirov@gmail.com>
2022-08-15 23:38:08 +0200

    LEFT JOIN, RIGHT JOIN

--- join_northwind.sql
+++ join_northwind.sql
@@ -1,5 +1,7 @@
 -- Active: 1660408859573@@127.0.0.1@5432@northwind
+--* ---------------------------------------------------------------------
 --* INNER JOIN (JOIN)
+--* ---------------------------------------------------------------------
 
 SELECT product_name, suppliers.company_name, units_in_stock
 FROM products
@@ -29,3 +31,43 @@ SELECT order_date, product_name, ship_country, products.unit_price, quantity, di
 FROM orders
 INNER JOIN order_details ON orders.order_id = order_details.order_id
 INNER JOIN products ON order_details.product_id = products.product_id;
+
+
+
+--* ---------------------------------------------------------------------
+--* LEFT JOIN, RIGHT JOIN
+--* ---------------------------------------------------------------------
+
+SELECT company_name, product_name
+FROM suppliers
+LEFT JOIN products ON suppliers.supplier_id = products.supplier_id;
+
+SELECT company_name, order_id
+FROM customers
+JOIN orders ON orders.customer_id = customers.customer_id
+WHERE order_id IS NULL;
+
+SELECT company_name, order_id
+FROM customers
+LEFT JOIN orders ON orders.customer_id = customers.customer_id
+WHERE order_id IS NULL;
+
+SELECT COUNT(*)
+FROM customers
+LEFT JOIN orders ON orders.customer_id = customers.customer_id
+WHERE order_id IS NULL;
+
+SELECT last_name, order_id
+FROM employees
+LEFT JOIN orders ON orders.employee_id = employees.employee_id
+WHERE order_id IS NULL;
+
+SELECT COUNT(*)
+FROM employees
+LEFT JOIN orders ON orders.employee_id = employees.employee_id
+WHERE order_id IS NULL;
+
+SELECT company_name, order_id
+FROM orders
+RIGHT JOIN customers ON orders.customer_id = customers.customer_id
+WHERE order_id IS NULL;
