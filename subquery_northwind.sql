17c5fff0c944f93699b5e7a240e31bba800d5afc
Dmytro Kagirov <dmytro27kagirov@gmail.com>
2022-08-18 00:11:43 +0200

    AVG with subquery was added.

--- subquery_northwind.sql
+++ subquery_northwind.sql
@@ -33,3 +33,12 @@ LEFT JOIN products USING(category_id)
 GROUP BY category_name
 ORDER BY units_in_stock DESC
 LIMIT (SELECT MIN(product_id) + 4 FROM products);
+
+SELECT AVG(units_in_stock)
+FROM products;
+
+SELECT product_name, units_in_stock
+FROM products
+WHERE units_in_stock > (SELECT AVG(units_in_stock)
+                        FROM products)
+ORDER BY units_in_stock;
