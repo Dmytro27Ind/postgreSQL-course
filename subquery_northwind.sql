c395918b93b5e1224c3e5850bd681330a6b9cae9
Dmytro Kagirov <dmytro27kagirov@gmail.com>
2022-08-18 00:05:24 +0200

    subquery category_name

--- subquery_northwind.sql
+++ subquery_northwind.sql
@@ -1,1 +1,35 @@
 -- Active: 1660408859573@@127.0.0.1@5432@northwind
+
+SELECT DISTINCT country
+FROM customers;
+
+SELECT company_name
+FROM suppliers
+WHERE country IN ('Argentina', 'Spain', '... and other');
+
+SELECT DISTINCT company_name
+FROM suppliers
+WHERE country IN   (SELECT DISTINCT country
+                    FROM customers);
+
+SELECT DISTINCT suppliers.company_name
+FROM suppliers
+INNER JOIN customers USING(country);
+
+SELECT DISTINCT suppliers.company_name
+FROM suppliers
+JOIN customers ON suppliers.country = customers.country;
+
+SELECT category_name, SUM(units_in_stock) AS units_in_stock
+FROM categories
+LEFT JOIN products USING(category_id)
+GROUP BY category_name
+ORDER BY units_in_stock DESC
+LIMIT (SELECT MIN(product_id) FROM products) + 4;
+
+SELECT category_name, SUM(units_in_stock) AS units_in_stock
+FROM categories
+LEFT JOIN products USING(category_id)
+GROUP BY category_name
+ORDER BY units_in_stock DESC
+LIMIT (SELECT MIN(product_id) + 4 FROM products);
