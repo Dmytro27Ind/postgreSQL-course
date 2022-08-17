e3125256ecb833026e61fcb476b103c50adc1a34
Dmytro Kagirov <dmytro27kagirov@gmail.com>
2022-08-18 00:33:34 +0200

    WHERE EXISTS, WHERE NOT EXISTS

--- subquery_northwind.sql
+++ subquery_northwind.sql
@@ -42,3 +42,20 @@ FROM products
 WHERE units_in_stock > (SELECT AVG(units_in_stock)
                         FROM products)
 ORDER BY units_in_stock;
+
+
+--* ---------------------------------------------------------------------
+--* WHERE EXISTS, WHERE NOT EXISTS
+--* ---------------------------------------------------------------------
+
+SELECT company_name, contact_name
+FROM customers
+WHERE EXISTS (  SELECT customer_id FROM orders
+                WHERE customer_id = customers.customer_id
+                AND freight BETWEEN 50 AND 100);
+
+SELECT company_name, contact_name
+FROM customers
+WHERE NOT EXISTS (  SELECT customer_id FROM orders
+                WHERE customer_id = customers.customer_id
+                AND order_date BETWEEN '1995-02-01' AND '1995-02-15');
