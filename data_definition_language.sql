190a28bf40694d3cde167f85af1fe14ebbfc3176
Dmytro Kagirov <dmytro27kagirov@gmail.com>
2022-08-18 16:01:06 +0200

    CONSTRAINT

--- data_definition_language.sql
+++ data_definition_language.sql
@@ -61,3 +61,61 @@ SELECT * FROM faculty;
 TRUNCATE TABLE faculty;                         --* restart only table
 TRUNCATE TABLE faculty RESTART IDENTITY;        --* restart table and restart identity (auto inckrement like SERIAL)
 
+DROP TABLE faculty;             --* delete table (drop table)
+
+DROP TABLE chair;
+CREATE TABLE chair
+(
+    chair_id    SERIAL  PRIMARY KEY,
+    chair_name  VARCHAR,
+    dean        VARCHAR
+);
+
+INSERT INTO chair
+VALUES (1, 'name', 'dean');
+
+SELECT * FROM chair;
+
+-- INSERT INTO chair
+-- VALUES (NULL, 'name2', 'dean2');        --* null value in PRIMARY KEY
+
+INSERT INTO chair
+VALUES (2, 'name2', 'dean2');
+
+
+DROP TABLE chair;
+CREATE TABLE chair
+(
+    chair_id    SERIAL  UNIQUE NOT NULL,        --* like PRIMARY KEY, but primary key only to one column for table
+    chair_name  VARCHAR,
+    dean        VARCHAR
+);
+
+SELECT constraint_name                          --* SELECT CONSTRAINT name of column
+FROM information_schema.key_column_usage
+WHERE table_name = 'chair'
+    AND table_schema = 'public'
+    AND column_name = 'chair_id';
+
+
+DROP TABLE chair;
+CREATE TABLE chair
+(
+    chair_id    SERIAL,
+    chair_name  VARCHAR,
+    dean        VARCHAR,
+
+    CONSTRAINT pk_chair_chair_id PRIMARY KEY(chair_id)          --* add CONSTRAINT
+);
+
+ALTER TABLE chair
+DROP CONSTRAINT pk_chair_chair_id;          --* drop CONSTRAINT
+
+ALTER TABLE chair
+ADD PRIMARY KEY(chair_id);                  --* add PRIMARY KEY
+
+SELECT constraint_name                          --* SELECT CONSTRAINT name of column
+FROM information_schema.key_column_usage
+WHERE table_name = 'chair'
+    AND table_schema = 'public'
+    AND column_name = 'chair_id';
